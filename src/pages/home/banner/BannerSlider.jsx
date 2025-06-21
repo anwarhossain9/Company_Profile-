502fc83175f8928e85e2aa0d62f908d64618caed
ripon106 <riponsheikh106@gmail.com>
2025-06-21 15:30:23 +0600

    slider added

--- src/pages/home/banner/BannerSlider.jsx
+++ src/pages/home/banner/BannerSlider.jsx
@@ -1,11 +1,52 @@
-import React from 'react'
+import React from "react"
+import { useKeenSlider } from "keen-slider/react"
+import "keen-slider/keen-slider.min.css"
 
-function BannerSlider() {
-    return (
-        <div>
-            <img className='w-full' src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR60KD1FLVGxNjncnxyHR2XYjmu_VdAJ_dNCw&s" alt="" />
-        </div>
-    )
+export default () => {
+  const [sliderRef] = useKeenSlider(
+    {
+      loop: true,
+    },
+    [
+      (slider) => {
+        let timeout
+        let mouseOver = false
+        function clearNextTimeout() {
+          clearTimeout(timeout)
+        }
+        function nextTimeout() {
+          clearTimeout(timeout)
+          if (mouseOver) return
+          timeout = setTimeout(() => {
+            slider.next()
+          }, 2000)
+        }
+        slider.on("created", () => {
+          slider.container.addEventListener("mouseover", () => {
+            mouseOver = true
+            clearNextTimeout()
+          })
+          slider.container.addEventListener("mouseout", () => {
+            mouseOver = false
+            nextTimeout()
+          })
+          nextTimeout()
+        })
+        slider.on("dragStarted", clearNextTimeout)
+        slider.on("animationEnded", nextTimeout)
+        slider.on("updated", nextTimeout)
+      },
+    ]
+  )
+
+  return (
+    <>
+      <div ref={sliderRef} className="keen-slider">
+        <div className="keen-slider__slide"><img className="w-full h-[250px]" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQRtyR3_sCX3Wykik-F1_IFir4fylyGuzDHgw&s" alt="" /></div>
+        <div className="keen-slider__slide"><img className="w-full h-[250px]" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT90vnfhupbIMjrWYiYNhE606hbX-F853jckQ&s" alt="" /></div>
+        <div className="keen-slider__slide"><img className="w-full h-[250px]" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS9xUI945Eqj3DNLh4LX2fmQSCLm7HaZoKqNA&s" alt="" /></div>
+        <div className="keen-slider__slide"><img className="w-full h-[250px]" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQcYg3pJghR0SzLmMM1pHiDiC3vif_cFfW8iw&s" alt="" /></div>
+      </div>
+    </>
+  )
 }
-
-export default BannerSlider
